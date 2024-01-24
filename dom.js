a5936e514c03229f945e75be5ba8c6cdb34c3e8c
abdulwasaysiddiqui <abdulwasaysiddiqui704@gmail.com>
2024-01-24 18:55:07 +0500

    dom imp

--- dom.js
+++ dom.js
@@ -1,10 +1,37 @@
-// DOM
+//        DOCUMENT OBJECT MODLE
 
-let word = document.getElementById('para');
-word.innerHTML = "KESY HO";
-word.style.backgroundColor = "cyan";
-// word.id = "world";
-// word.classList.add("HELLO");
+// let word = document.getElementById('para');
+// word.innerHTML = "KESY HO";
+// word.style.backgroundColor = "cyan";
+// // word.id = "world";
+// // word.classList.add("HELLO");
 
 
-console.log(word);
+// console.log(word);
+
+let elem = document.getElementById('second');
+// console.log(elem);
+// elem.innerHTML = "AN APPLE A DAY!"
+elem.textContent = "AN APPLE A DAY!"
+elem.style.backgroundColor = "yellow";
+elem.style.textAlign = "center";
+elem.style.padding = "20px";
+elem.classList.add('two');
+elem.classList.remove('two');
+
+
+
+let myDiv = document.getElementsByClassName('container');
+myDiv[0].innerHTML = "BATCH CODE!"
+myDiv[0].style.backgroundColor = "purple"
+myDiv[0].style.color = "white";
+myDiv[0].style.textAlign = "center";
+myDiv[0].style.fontSize = "40px";
+myDiv[0].classList.add('three');
+
+let newElem = document.createElement('p');
+myDiv[0].appendChild(newElem);
+newElem.innerHTML = "BATCH CODE!"
+let pencil = document.createElement('img');
+newElem.appendChild(pencil);
+pencil.src = "1.png";
